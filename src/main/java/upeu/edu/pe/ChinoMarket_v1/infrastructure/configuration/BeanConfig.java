e1dcb494eb3dc08cabcc02b01ed93ffda559ae04
Benroj0 <>
2024-09-30 15:53:54 -0500

    adwawdawd

--- src/main/java/upeu/edu/pe/ChinoMarket_v1/infrastructure/configuration/BeanConfig.java
+++ src/main/java/upeu/edu/pe/ChinoMarket_v1/infrastructure/configuration/BeanConfig.java
@@ -2,22 +2,10 @@ package upeu.edu.pe.ChinoMarket_v1.infrastructure.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.context.WebApplicationContext;
-import upeu.edu.pe.ChinoMarket_v1.app.repository.OrderProductRepository;
-import upeu.edu.pe.ChinoMarket_v1.app.repository.OrderRepository;
 import upeu.edu.pe.ChinoMarket_v1.app.repository.ProductRepository;
 import upeu.edu.pe.ChinoMarket_v1.app.repository.StockRepository;
 import upeu.edu.pe.ChinoMarket_v1.app.repository.UserRepository;
-import upeu.edu.pe.ChinoMarket_v1.app.service.CarritoService;
-import upeu.edu.pe.ChinoMarket_v1.app.service.LoginService;
-import upeu.edu.pe.ChinoMarket_v1.app.service.LogoutService;
-import upeu.edu.pe.ChinoMarket_v1.app.service.OrderProductService;
-import upeu.edu.pe.ChinoMarket_v1.app.service.OrderService;
 import upeu.edu.pe.ChinoMarket_v1.app.service.ProductService;
-import upeu.edu.pe.ChinoMarket_v1.app.service.RegisterService;
 import upeu.edu.pe.ChinoMarket_v1.app.service.StockService;
 import upeu.edu.pe.ChinoMarket_v1.app.service.UploadFile;
 import upeu.edu.pe.ChinoMarket_v1.app.service.UserService;
@@ -47,39 +35,8 @@ public class BeanConfig {
     }
 
     @Bean
-    public OrderService orderService(OrderRepository orderRepository) {
-        return new OrderService(orderRepository);
-    }
-
-    @Bean
-    public OrderProductService orderProductService(OrderProductRepository orderProductRepository) {
-        return new OrderProductService(orderProductRepository);
-    }
-
-    @Bean
-    @Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
-    public CarritoService carritoService() {
-        return new CarritoService();
-    }
-
-    @Bean
     public UserService userService(UserRepository userRepository) {
         return new UserService(userRepository);
-    }
-
-    @Bean
-    public LoginService loginService(UserService userService) {
-        return new LoginService(userService);
-    }
-
-    @Bean
-    public LogoutService logoutService() {
-        return new LogoutService();
-    }
-
-    @Bean
-    public RegisterService registrationService(UserService userService, PasswordEncoder passwordEncoder) {
-        return new RegisterService(userService, passwordEncoder);
     }
 
 }
