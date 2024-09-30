3797a11e5e8c2340bc211e78dec44780710d7f8f
Benroj0 <Administrador@Usuario>
2024-09-30 16:14:35 -0500

    adwawdawd

--- src/main/java/upeu/edu/pe/ChinoMarket_v1/infrastructure/controller/ProductController.java
+++ src/main/java/upeu/edu/pe/ChinoMarket_v1/infrastructure/controller/ProductController.java
@@ -1,6 +1,5 @@
 package upeu.edu.pe.ChinoMarket_v1.infrastructure.controller;
 
-import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import org.slf4j.*;
 import org.springframework.stereotype.Controller;
@@ -9,60 +8,55 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import upeu.edu.pe.ChinoMarket_v1.app.service.ProductService;
 import upeu.edu.pe.ChinoMarket_v1.infrastructure.entity.ProductEntity;
 import upeu.edu.pe.ChinoMarket_v1.infrastructure.entity.UserEntity;
 
 @Controller
-@RequestMapping("admin/product")
+@RequestMapping("/admin/products") //localhost:8080/
 public class ProductController {
 
-    private final ProductService productService; 
+    private final ProductService productService;
     private final Logger log = LoggerFactory.getLogger(ProductController.class);
 
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
-    // Crear nuevo producto
     @GetMapping("/create")
-    public String create(){
+    public String create() {
         return "admin/products/create";
-       }
-
-    // Guardar producto
-    @PostMapping("/saveProduct")
-    public String saveProduct(ProductEntity product, @RequestParam("img") MultipartFile multipartFile) throws IOException {
-        log.info("Nombre del producto {}", product);
-        productService.saveProduct(product, multipartFile);
-        return "redirect:/admin";   
     }
-
-    // Mostrar productos
+    
     @GetMapping("/show")
-    public String showProduct(Model model){
+    public String showProduct(Model model) {
         UserEntity userEntity = new UserEntity();
         userEntity.setId(1);
         Iterable<ProductEntity> products = productService.getProductsByUser(userEntity);
-        model.addAttribute("products", products); 
+        model.addAttribute("products", products);
         return "admin/products/show";
     }
-
-    // Editar producto
+    
+    @PostMapping("/saveProduct")
+    public String saveProduct(ProductEntity product, MultipartFile multipartfile) throws IOException{
+        log.info("nombre del product:{}", product);
+        productService.saveProduct(product, multipartfile);
+        return "redirect:/admin";
+    }
+    
     @GetMapping("/edit/{id}")
-    public String editProduct(@PathVariable Integer id, Model model){
+    public String editProduct(@PathVariable Integer id, Model model) {
         ProductEntity product = productService.getProductById(id);
-        log.info("Producto obtenido: {}", product);
+        log.info("nombre del product:{}", product);
         model.addAttribute("product", product);
         return "admin/products/edit";
     }
-
-    // Eliminar producto
+    
     @GetMapping("/delete/{id}")
-    public String deleteProduct(@PathVariable Integer id){
+    public String deleteProduct(@PathVariable Integer id) {
         productService.deleteProductById(id);
-        return "redirect:/admin";
+        return "redirect:/admin/products";
     }
+
 }
--- src/main/java/upeu/edu/pe/ChinoMarket_v1/infrastructure/controller/ProductControllerApi.java
+++ src/main/java/upeu/edu/pe/ChinoMarket_v1/infrastructure/controller/ProductControllerApi.java
@@ -1,54 +1,72 @@
 package upeu.edu.pe.ChinoMarket_v1.infrastructure.controller;
 
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import upeu.edu.pe.ChinoMarket_v1.app.service.ProductService;
 import upeu.edu.pe.ChinoMarket_v1.infrastructure.entity.ProductEntity;
 import upeu.edu.pe.ChinoMarket_v1.infrastructure.entity.UserEntity;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://4200")
 @RestController
-@RequestMapping("apiProduct") //RUTA
-
+@RequestMapping("api") //localhost:8080/
 public class ProductControllerApi {
 
     private final ProductService productService;
-
+    private final Logger log = LoggerFactory.getLogger(ProductControllerApi.class);
 
     public ProductControllerApi(ProductService productService) {
         this.productService = productService;
     }
-
-    @PostMapping("/saveProduct")
-    public String saveProduct(@RequestBody ProductEntity productEntity) throws IOException {
-    return productService.saveProductApi(productEntity).toString();
-    }
     
-    // Mostrar productos
-    @GetMapping("/show") 
+    //Ver productos
+    @GetMapping("/show")
+    @ResponseStatus(HttpStatus.OK)
     public Iterable<ProductEntity> showProduct() {
         UserEntity user = new UserEntity();
         user.setId(1);
         return productService.getProductsByUser(user);
     }
 
-    // Editar producto
-    @PutMapping("/edit/{id}")
+    
+    //crear product
+    @PostMapping("/product")
     @ResponseStatus(HttpStatus.CREATED)
-    public ProductEntity editProduct(@RequestBody ProductEntity product, @PathVariable Integer id) throws IOException {
+    public ProductEntity createProduct(@RequestBody ProductEntity product, MultipartFile multipartfile) throws IOException {
+        return productService.saveProduct(product, multipartfile);
+    }
+    //buscar producto por Id
+    @GetMapping("/product/{id}")
+    public ProductEntity show(@PathVariable Integer id) {
+        return productService.getProductById(id);
+    }
+    
+    //editar un product
+    @PutMapping("/product/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductEntity editProduct(@RequestBody ProductEntity product, @PathVariable Integer id, MultipartFile multipartfile) throws IOException {
         ProductEntity productActual = productService.getProductById(id);
         productActual.setDescripcion(product.getDescripcion());
         productActual.setNombre(product.getNombre());
         productActual.setPrecio(product.getPrecio());
         productActual.setUserEntity(product.getUserEntity());
-        return productService.saveProductApi(productActual);
+        return productService.saveProduct(productActual, multipartfile);
+        // log.info("Product obtenido: {}", product);
+        //model.addAttribute("product", product);
+        //return "admin/products/edit";
     }
 
-    // Eliminar producto
-    @DeleteMapping("/delete/{id}")
+    //eliminar un product
+    @DeleteMapping("/product/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteProduct(@PathVariable Integer id) {
+    public boolean deleteProduct(@PathVariable Integer id) {
         productService.deleteProductById(id);
+        // return "redirect:/admin/products/show";
+        return true;
     }
+    
+
 }
